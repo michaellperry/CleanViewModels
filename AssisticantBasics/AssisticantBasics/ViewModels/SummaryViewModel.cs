aba0fdbf257455e4e7be9f586ba0543eb360b251
Michael L Perry <michael@qedcode.com>
2014-07-18 21:57:07 -0500

    Added summary view model to the view model locator.

---
+++ AssisticantBasics/AssisticantBasics/ViewModels/SummaryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AssisticantBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssisticantBasics.ViewModels
+{
+    public class SummaryViewModel
+    {
+        private readonly Document _document;
+
+        public SummaryViewModel(Document document)
+        {
+            _document = document;
+        }
+
+        public int Total
+        {
+            get { return _document.Total; }
+        }
+    }
+}
