bab657955786cd6f1e2e8d993ca6a8afefb5a507
Michael L Perry <michael@qedcode.com>
2014-07-18 21:08:33 -0500

    Added observable quantity.

--- AssisticantBasics/AssisticantBasics/Models/Item.cs
+++ AssisticantBasics/AssisticantBasics/Models/Item.cs
@@ -9,12 +9,21 @@ namespace AssisticantBasics.Models
 {
     public class Item
     {
-        private Observable<string> _name = new Observable<string>();
+        private Observable<string> _name =
+            new Observable<string>();
+        private Observable<int> _quantity =
+            new Observable<int>();
 
         public string Name
         {
             get { return _name; }
             set { _name.Value = value; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity.Value = value; }
         }
     }
 }
