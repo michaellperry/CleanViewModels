a3b99c63e7297e3b23d6df32ae005a8e2266affd
Michael L Perry <michael@qedcode.com>
2014-08-02 12:53:08 -0500

    Added namespaces.

--- PierceWrapper/PierceWrapper/Services/DialogManager.cs
+++ PierceWrapper/PierceWrapper/Services/DialogManager.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PierceWrapper.Models;
+using PierceWrapper.Views;
+using PierceWrapper.ViewModels;
+using Assisticant;
 
 namespace PierceWrapper.Services
 {
--- PierceWrapper/PierceWrapper/Views/MainWindow.xaml.cs
+++ PierceWrapper/PierceWrapper/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assisticant;
+using PierceWrapper.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
