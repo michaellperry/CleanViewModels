637f2ad16863666cf5c02f6e1430c14dde1c15d3
Michael L Perry <michael@qedcode.com>
2014-08-02 14:27:46 -0500

    Factor the contact card view out of the main window.

--- PierceWrapper/PierceWrapper/MainWindow.xaml.cs
+++ PierceWrapper/PierceWrapper/MainWindow.xaml.cs
@@ -12,10 +12,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PierceWrapper.Views
+namespace PierceWrapper
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
---
+++ PierceWrapper/PierceWrapper/Pages/MainPage.cs
@@ -0,0 +1,28 @@
+﻿using Assisticant;
+using PierceWrapper.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PierceWrapper.Services;
+using Assisticant.Fields;
+
+namespace PierceWrapper.Pages
+{
+    public class MainPage : ViewModelBase
+    {
+        private ContactRepository _repository = null;
+        private DialogManager _dialogManager = null;
+        private Observable<int> _contactId = new Observable<int>();
+
+        public object Content
+        {
+            get
+            {
+                return Get(() => ForView.Wrap(
+                    new ContactCardViewModel(_repository, _dialogManager, _contactId)));
+            }
+        }
+    }
+}
