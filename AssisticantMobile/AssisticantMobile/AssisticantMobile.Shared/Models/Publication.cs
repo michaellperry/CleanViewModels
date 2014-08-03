2ac89aceec1f9252f0d590f963920946080481b5
Michael L Perry <michael@qedcode.com>
2014-08-03 15:15:37 -0500

    No need to lock if using UI synchronization context.

--- AssisticantMobile/AssisticantMobile/AssisticantMobile.Shared/Models/Publication.cs
+++ AssisticantMobile/AssisticantMobile/AssisticantMobile.Shared/Models/Publication.cs
@@ -26,27 +26,24 @@ namespace AssisticantMobile.Models
         {
             await Task.Delay(2000);
 
-            lock (this)
+            _articles.Add(new Article
             {
-                _articles.Add(new Article
-                {
-                    Author = "Jaynes, E. T.",
-                    Title = "Information Theory and Statistical Mechanics",
-                    Date = new DateTime(1957, 5, 10)
-                });
-                _articles.Add(new Article
-                {
-                    Author = "Reza, Fazlollah M.",
-                    Title = "An Introduction to Information Theory",
-                    Date = new DateTime(1961, 3, 14)
-                });
-                _articles.Add(new Article
-                {
-                    Author = "Gibson, Jerry D.",
-                    Title = "Digital Compression for Multimedia: Principles and Standards",
-                    Date = new DateTime(1998, 12, 3)
-                });
-            }
+                Author = "Jaynes, E. T.",
+                Title = "Information Theory and Statistical Mechanics",
+                Date = new DateTime(1957, 5, 10)
+            });
+            _articles.Add(new Article
+            {
+                Author = "Reza, Fazlollah M.",
+                Title = "An Introduction to Information Theory",
+                Date = new DateTime(1961, 3, 14)
+            });
+            _articles.Add(new Article
+            {
+                Author = "Gibson, Jerry D.",
+                Title = "Digital Compression for Multimedia: Principles and Standards",
+                Date = new DateTime(1998, 12, 3)
+            });
         }
     }
 }
