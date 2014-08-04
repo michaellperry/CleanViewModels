ddc04d5e680080bbb30d210550acb535e100c1eb
Michael L Perry <michael@qedcode.com>
2014-08-04 00:02:28 -0500

    Update coordinate in background.

--- AssisticantMobile/AssisticantMobile/AssisticantMobile.Shared/Services/GpsLocationService.cs
+++ AssisticantMobile/AssisticantMobile/AssisticantMobile.Shared/Services/GpsLocationService.cs
@@ -13,6 +13,15 @@ namespace AssisticantMobile.Services
         {
             _geolocator = new Geolocator();
             _geolocator.MovementThreshold = 3.0;
+            _geolocator.PositionChanged += _geolocator_PositionChanged;
+        }
+
+        void _geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            lock (this)
+            {
+                _coordinate.Value = args.Position.Coordinate;
+            }
         }
 
         public Geocoordinate Coordinate
