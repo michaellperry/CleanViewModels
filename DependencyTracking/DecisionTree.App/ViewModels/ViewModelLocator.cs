c571c1a75d6e17f56a8bdb2c5103fcad08ee08af
Michael L Perry <michael@qedcode.com>
2014-08-19 22:09:57 -0500

    Decision tree app.

---
+++ DependencyTracking/DecisionTree.App/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,60 @@
+using Assisticant;
+using DecisionTree.App.Models;
+
+namespace DecisionTree.App.ViewModels
+{
+    public class ViewModelLocator : ViewModelLocatorBase
+    {
+        private Document _document;
+		private Selection _selection;
+
+        public ViewModelLocator()
+        {
+			if (DesignMode)
+				_document = LoadDesignModeDocument();
+			else
+				_document = LoadDocument();
+			_selection = new Selection();
+        }
+
+        public object Main
+        {
+            get { return ViewModel(() => new MainViewModel(_document, _selection)); }
+        }
+
+		public object Item
+		{
+			get
+			{
+				return ViewModel(() => _selection.SelectedItem == null
+					? null
+					: new ItemViewModel(_selection.SelectedItem));
+			}
+		}
+
+		private Document LoadDocument()
+		{
+			// TODO: Load your document here.
+            Document document = new Document();
+            var one = document.NewItem();
+            one.Name = "One";
+            var two = document.NewItem();
+            two.Name = "Two";
+            var three = document.NewItem();
+            three.Name = "Three";
+            return document;
+		}
+
+		private Document LoadDesignModeDocument()
+		{
+            Document document = new Document();
+            var one = document.NewItem();
+            one.Name = "Design";
+            var two = document.NewItem();
+            two.Name = "Mode";
+            var three = document.NewItem();
+            three.Name = "Data";
+            return document;
+		}
+    }
+}
