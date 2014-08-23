10630cb147fce7504f0384a6f7f656bb2b24c236
Michael L Perry <michael@qedcode.com>
2014-08-22 22:05:17 -0500

    Avoid circular dependencies.

---
+++ DependencyTracking/Gotchas/AvoidCircularDependencies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gotchas.Examples;
+
+namespace Gotchas
+{
+    [TestClass]
+    public class AvoidCircularDependencies
+    {
+        [TestMethod]
+        public void CanComputeLoan()
+        {
+            var loan = new LoanCalculator();
+            loan.Principal = 1000.0;
+            loan.Rate = 0.08;
+            //loan.Time = 5;
+
+            var total = loan.Total;
+
+            Assert.AreEqual(1100.0, total);
+        }
+    }
+}
---
+++ DependencyTracking/Gotchas/Examples/LoanCalculator.cs
@@ -0,0 +1,59 @@
+﻿using Assisticant.Fields;
+using System;
+
+namespace Gotchas.Examples
+{
+    public class LoanCalculator
+    {
+        // T = P (1 + r/n)^(nt)
+        // T = Total
+        // P = Principal
+        // r = Interest rate
+        // n = Periods per year (12)
+        // t = Number of years
+
+        // ln(T) = nt ln(P (1 + r/n))
+        // t = ln(T) / [n ln(P (1 + r/n))]
+        private Observable<double> _principal = new Observable<double>();
+        private Observable<double> _rate = new Observable<double>();
+        private Computed<double> _total;
+        private Computed<double> _time;
+        private const double _periods = 12;
+
+        public LoanCalculator()
+        {
+            _total = new Computed<double>(() =>
+                _principal * Math.Pow(
+                    1.0 + _rate / _periods,
+                    _periods * _time)
+            );
+            _time = new Computed<double>(() =>
+                Math.Log(_total) /
+                (_rate * Math.Log(_principal *
+                    (1.0 + _rate / _periods)))
+            );
+        }
+
+        public double Principal
+        {
+            get { return _principal; }
+            set { _principal.Value = value; }
+        }
+
+        public double Rate
+        {
+            get { return _rate; }
+            set { _rate.Value = value; }
+        }
+
+        public double Total
+        {
+            get { return _total.Value; }
+        }
+
+        public double Time
+        {
+            get { return _time.Value; }
+        }
+    }
+}
